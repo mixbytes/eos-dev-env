bbd8aceaecd20c6806ada0fc05a542694cb782d9
Algys Ievlev <algys.ievlev@gmail.com>
2018-05-06 14:03:09 +0300

    fix incorrct path

--- eos-dev-env.sh
+++ eos-dev-env.sh
@@ -3,7 +3,7 @@ EOS_ENV_DIR=$HOME/opt/eos-dev
 if [[ $1 = "up" ]]
 then
     echo 'Setting UP EOS dev environment...'
-    alias cleos="docker-compose -f $EOS_ENV_DIR/docker/docker-compose-dawn3.0.yaml exec keosd /opt/eosio/bin/cleos -u http://docker_nodeosd_1"
+    alias cleos="docker-compose -f $EOS_ENV_DIR/eos-compile-service/docker/docker-compose-dawn3.0.yaml exec keosd /opt/eosio/bin/cleos -u http://docker_nodeosd_1"
     alias eos-deploy="$EOS_ENV_DIR/eos-deploy/eos-deploy.js"
     alias eos-compile="$EOS_ENV_DIR/eos-compile-service/main.js --compiler-bin $EOS_ENV_DIR/eos-compile-service/compile-eosio-contract/compile-eosio-contract"
 elif [[ $1 = "down" ]]
